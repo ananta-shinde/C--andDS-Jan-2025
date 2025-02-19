b04da420970f18fd27fc4a73de8ca6bc421e9fc9
aShinde <ananta.shinde@honeywellcom>
2025-02-19 09:16:40 +0530

    19/02/2025

--- structure/demo01.cpp
+++ structure/demo01.cpp
@@ -5,7 +5,10 @@ struct Student
    int rollNo;
    float phy;
    float chem;
-   float math;
+   float math; 
+   float obtained_total;
+   float percentage;
+   int rank = 0;
 };
 
 int main(int argc, char const *argv[])
@@ -21,12 +24,29 @@ int main(int argc, char const *argv[])
         scanf("%f",&stud_list[i].chem);
         printf("Enter marks of math:");
         scanf("%f",&stud_list[i].math);
-   }
+        float obatinedMarks = stud_list[i].phy+stud_list[i].chem+stud_list[i].math;
+        float percentage = (obatinedMarks/300)*100;
+        stud_list[i].obtained_total = obatinedMarks;
+        stud_list[i].percentage = percentage;
+   }   
 
+   int current_rank = 1;
+   for(int j=0;j<5;j++){
+        int largestIndex = 0;
+        for(int i=0;i<5;i++){
+           if(stud_list[i].rank ==0 && stud_list[i].obtained_total > stud_list[largestIndex].obtained_total)
+           {
+               largestIndex = i;
+           }
+        }
+        stud_list[largestIndex].rank = current_rank;
+        current_rank++;
+   }
+   
    printf("\n************************************\n");
-   printf("roll no      physics     chemistry      maths\n");
+   printf("roll no      physics     chemistry      maths     obtained      percentage\n");
    for(int i=0;i<5;i++){
-        printf("%d     %f     %f     %f\n",stud_list[i].rollNo,stud_list[i].phy,stud_list[i].chem,stud_list[i].math);
+        printf("%d     %f     %f     %f    %f    %f   %d\n",stud_list[i].rollNo,stud_list[i].phy,stud_list[i].chem,stud_list[i].math,stud_list[i].obtained_total,stud_list[i].percentage,stud_list[i].rank);
    }
    
 
